a1448d2810ed54db8ebf6c70204d0ff9cad661dd
EmilLaptop <emil.raedsch@t-online.de>
2022-12-28 23:22:36 +1000

    add FindPenguins Map

--- pages/australia/index.js
+++ pages/australia/index.js
@@ -1,6 +1,7 @@
 import SectionCover from "../../components/SectionCover";
 import MyImage from "../../components/my/MyImage";
 import MainButton from "../../components/buttons/MainButton";
+import Heading1 from "../../components/typography/Heading1";
 
 import styles from "../../styles/pages/australia/australiaHome.module.scss";
 import images from "../../data/images";
@@ -33,6 +34,26 @@ const AustraliaHome = ({ posts, data }) => {
                     />
                 </div>
             </SectionCover>
+            <section className={styles.map}>
+                <Heading1 title="Where am I now?" />
+                <div
+                    class="findpenguins-media"
+                    findpgns-version="1"
+                    findpgns-embed="user"
+                >
+                    <a
+                        href="https://findpenguins.com/65bnkkro5pm6v/trip/australia-work-and-travel"
+                        target="_top"
+                    >
+                        Map
+                    </a>
+                </div>
+                <script
+                    async
+                    defer
+                    src="//findpenguins.com/js/embed.js"
+                ></script>
+            </section>
         </>
     );
 };
