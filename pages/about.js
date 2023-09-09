fd0868e11f9a165a9e9ea0ea9e5cab0a2a9f005c
EmilLaptop <emil.raedsch@t-online.de>
2023-09-09 16:37:21 +0930

    fix

--- pages/about.js
+++ pages/about.js
@@ -12,24 +12,24 @@ const About = (props) => {
             <main className={styles.content}>
                 <Heading1 title="What and Why" />
                 <p className={styles.content__text}>
-                    This isn't the typical travel-blog focused on making money,
-                    with guides, tips and tricks and stuff like that (That said,
-                    I might try to earn a few bucks off this at some point lol).
-                    This is a different kind of blog - somewhere where I just
-                    write about things in my head, whenever I feel like writing
-                    them down.
+                    This isn&apos;t the typical travel-blog focused on making
+                    money, with guides, tips and tricks and stuff like that
+                    (That said, I might try to earn a few bucks off this at some
+                    point lol). This is a different kind of blog - somewhere
+                    where I just write about things in my head, whenever I feel
+                    like writing them down.
                     <br />
                     Why do I do this? Honestly, I was thinking about this quite
                     a while - at some point, a few months before going to
                     Australia, I just felt like starting a blog, and the idea
                     kind of just kept being on my mind - so I started building
-                    it. This wasn't a continuous progress at all, there where
-                    weeks in a row where I didn't do anything for the website.
-                    Even now, as I am writing this, the blog isn't nearly done;
-                    I haven't even finished the first post. I am writing this so
-                    that everybody who happens to come across this, knows what
-                    it is and more importantly, why it exists. To come to an
-                    conclusion:
+                    it. This wasn&apos;t a continuous progress at all, there
+                    where weeks in a row where I didn&apos;t do anything for the
+                    website. Even now, as I am writing this, the blog isn&apos;t
+                    nearly done; I haven't even finished the first post. I am
+                    writing this so that everybody who happens to come across
+                    this, knows what it is and more importantly, why it exists.
+                    To come to an conclusion:
                     <br />
                     <br />
                     This is a random blog about things shooting around on my
